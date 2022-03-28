642575e2a717d4a8a1a2d624358cf9affe0fbb01
Oleg Sheleg <sheleg.om@gmail.com>
2022-03-28 12:14:29 +1000

    firstcommit

---
+++ п10.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int user,a,rev;
+
+cin>>user;
+
+while(user>0){
+a=user%10;
+rev=rev*10+a;
+user=user/10;
+}
+cout<<rev<<endl;
+return 0;
+}
